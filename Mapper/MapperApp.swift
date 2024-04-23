a2e73a724815f85e1b4d7934e59f2d969ace9e39
Imran Razak <77019152+ImranRazak1@users.noreply.github.com>
2024-04-23 17:39:55 +0100

    Initial Commit

---
+++ Mapper/MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapperApp.swift
+//  Mapper
+//
+//  Created by Imran razak on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
